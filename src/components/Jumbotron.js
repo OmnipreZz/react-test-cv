aa692a4003e1ed893777cb51824fd4bad00fc25d
Omni <geoffrey.frioli@gmail.com>
2018-12-20 09:45:37 +0100

    first commit

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Jumbotron extends Component {
+  render() {
+    return (
+      <div className="Jumbotron">
+        <h1>Jumbotron</h1>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
