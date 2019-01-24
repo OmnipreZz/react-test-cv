b7315d34cb2ed5a681f4d2c92047b32dbb8ecce9
Omni <geoffrey.frioli@gmail.com>
2019-01-24 22:11:45 +0100

    footer button effect

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,19 @@ class Footer extends Component {
     return (
       <div className="Footer">
         <div className="FooterIcon">
+          <div className="OneIcon">
           <i className="fab fa-github"></i>
+          </div>
+          <div className="OneIcon">
           <i className="fab fa-linkedin-in"></i>
+          </div>
+          <div className="OneIcon">
           <i className="fab fa-twitter"></i>
+          </div>
+          <div className="OneIcon">
           <i className="fab fa-instagram"></i>
+          </div>
+
         </div>
       </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,30 +7,7 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
-        <p>
-
-Nisi quia accusamus dolores id. Blanditiis laudantium facere voluptas inventore iste consequatur. Rerum nihil ut consequuntur voluptatem. Ut consequatur reiciendis culpa vero maxime repudiandae tenetur voluptates.
-
-Consequuntur ad corporis sequi aut odio rerum ipsam iusto. Libero est rerum et facere maxime. Esse eaque perferendis eaque aspernatur.
-
-Voluptatem recusandae quae molestiae et non accusamus. Labore iure repudiandae vero. Culpa ad repudiandae vel quos vel aut optio. Cumque saepe molestiae tenetur.
-
-Doloribus adipisci debitis velit velit magnam. Illum quibusdam sunt architecto minus voluptatem omnis reprehenderit dolor. Enim mollitia non dolores iure voluptatem.
-
-Et aliquam totam quia sit perspiciatis. Ipsa deleniti rerum occaecati beatae aut quia illum. Id architecto molestias dolorem in sed aut illo officia. Inventore sed quidem ut nesciunt fugit excepturi deserunt aut. Eos voluptas impedit iusto rerum. Accusantium voluptatem et quas dicta eos.
-
-
-Nisi quia accusamus dolores id. Blanditiis laudantium facere voluptas inventore iste consequatur. Rerum nihil ut consequuntur voluptatem. Ut consequatur reiciendis culpa vero maxime repudiandae tenetur voluptates.
-
-Consequuntur ad corporis sequi aut odio rerum ipsam iusto. Libero est rerum et facere maxime. Esse eaque perferendis eaque aspernatur.
-
-Voluptatem recusandae quae molestiae et non accusamus. Labore iure repudiandae vero. Culpa ad repudiandae vel quos vel aut optio. Cumque saepe molestiae tenetur.
-
-Doloribus adipisci debitis velit velit magnam. Illum quibusdam sunt architecto minus voluptatem omnis reprehenderit dolor. Enim mollitia non dolores iure voluptatem.
-
-Et aliquam totam quia sit perspiciatis. Ipsa deleniti rerum occaecati beatae aut quia illum. Id architecto molestias dolorem in sed aut illo officia. Inventore sed quidem ut nesciunt fugit excepturi deserunt aut. Eos voluptas impedit iusto rerum. Accusantium voluptatem et quas dicta eos.
-</p>
+        
       </div>
     );
   }
